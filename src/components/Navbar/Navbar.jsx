3568170639d7bd2d563994245844e4f3417f73f1
Chi <chiyantsestudio@gmail.com>
2023-03-21 17:24:04 -0400

    nest all pages using createBrowerRouter as a children to Layout. Outlet is nest between Navbar and Footer within Layout. In NavBar, added an img, icons and item links"

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import SearchIcon from '@mui/icons-material/Search';
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+
+
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+        <div className="wrapper">
+            <div className="left">
+                <div className="item">
+                <img src="/img/en.png" alt="" />
+                    <KeyboardArrowDownIcon />
+                </div>
+                <div className="item">
+                    <span>USD</span>
+                    <KeyboardArrowDownIcon />
+                </div>
+                <div className="item">
+                    <Link to="/products/1">Women</Link>
+                </div>
+                <div className="item">
+                    <Link to="/products/2">Men</Link>
+                </div>
+                <div className="item">
+                    <Link to="/products/3">Childern</Link>
+                </div>
+                <div className="item">
+                    <Link to="/products/4">Accessories</Link>
+                </div>
+            </div>
+            <div className="center">
+                <Link to="/">TSE Store</Link>
+            </div>
+            <div className="right">
+                <div className="item">
+                    <Link to="/">HomgPage</Link>
+                </div>
+                <div className="item">
+                    <Link to="/">About</Link>
+                </div>
+                <div className="item">
+                    <Link to="/">Contact</Link>
+                </div>
+                <div className="icons">
+                    <SearchIcon />
+                    <PersonOutlineIcon />
+                    <FavoriteBorderOutlinedIcon />
+                    <ShoppingCartOutlinedIcon />
+                </div>
+                </div>      
+            </div>
+        </div>
+  )
+}
+
+export default Navbar
