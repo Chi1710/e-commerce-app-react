467e15b2948f789b87d54e5227c18c406ac39005
Chi <chiyantsestudio@gmail.com>
2023-03-21 23:26:43 -0400

    user can click on left and right icons to see the prev slide and next slide. the slider is in a loop

--- src/components/Slide/Slider.jsx
+++ src/components/Slide/Slider.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Slider.scss"
 import WestIcon from '@mui/icons-material/West';
 import EastIcon from '@mui/icons-material/East';
 
 const Slider = () => {
+
+    const [currentSlide, setCurrentSlide] = useState(0);
+
+    const prevSlide = () => {
+        setCurrentSlide(currentSlide === 0 ? 2 : (prev) =>prev - 1 );
+    }
+
+    const nextSlide = () => {
+        setCurrentSlide(currentSlide === 2 ? 0 : (prev) => prev + 1);
+    }
+
     const data = [
         "https://images.pexels.com/photos/1549200/pexels-photo-1549200.jpeg?auto=compress&cs=tinysrgb&w=1600",
         "https://images.pexels.com/photos/949670/pexels-photo-949670.jpeg?auto=compress&cs=tinysrgb&w=1600",
@@ -12,16 +23,16 @@ const Slider = () => {
 
   return (
     <div className='slider'>
-        <div className="container">
+        <div className="container" style={{transform:`translateX(-${currentSlide * 100}vw)`}}>
             <img src={data[0]} alt="" />
             <img src={data[1]} alt="" />
             <img src={data[2]} alt="" />
         </div>
         <div className="icons">
-            <div className="icon">
+            <div className="icon" onClick={prevSlide}>
                 <WestIcon />
             </div>
-            <div className="icon">
+            <div className="icon" onClick={nextSlide}>
                 <EastIcon />
             </div>
 
